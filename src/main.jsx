1bf98f30489e35596bb3b82892a30916352c00ba
frelss <originsupra@gmail.com>
2024-05-05 11:11:34 +0200

    Remove @fvilers package

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,6 @@ import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
-import { disableReactDevtools } from "@fvilers/disable-react-devtools";
-
-if (import.meta.env.VITE_MODE === "production") {
-  disableReactDevtools();
-}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
